33af5f1bce36bd993e6c107089c5725323ec4674
Alexander Gatland <alexander.gatland@no.experis.com>
2024-01-19 09:02:02 +0100

    Refactor computer class

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,25 +3,34 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    public ArrayList<Game> installedGames;
+    PowerSupply psu;
+
+    public Computer(PowerSupply psu) {
+        this.installedGames = new ArrayList<>();
+        this.psu = psu;
+    }
+
+    public Computer(PowerSupply psu, ArrayList<Game> installedGames) {
+        this.installedGames = installedGames;
+        this.psu = psu;
+    }
 
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        this.psu.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
-        this.installedGames.add(game);
+    public void installGame(String game) {
+        //Game game = new Game(game);
+        this.installedGames.add(new Game(game));
     }
 
-    public String playGame() {
+    public String playGame(String game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(game)) {
                 return g.start();
             }
         }
-
         return "Game not installed";
     }
 }
